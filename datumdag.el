f44761a6c63a041f4bff654ecb30fe5e7a709200
Henric Malmberg <henric@weld.io>
2019-03-02 22:37:34 +0100

    datumdag translated to swedish

--- datumdag.el
+++ datumdag.el
@@ -1,5 +1,23 @@
+;;; Datumdag --- insert specifically formatted timestamp
+;; Author: hanktard
+
+;;; Code:
+
 (defun datum-dag()
   "Print date and day."
-  (interactive) (insert (format-time-string "** %d/%m %a")))
+  (interactive)
+  (setq datum (format-time-string "** %d/%m %A"))
+  (setq datum (replace-regexp-in-string "0\d*" "" datum))
+  (setq datum (replace-regexp-in-string "Monday" "Måndag" datum))
+  (setq datum (replace-regexp-in-string "Tuesday" "Tisdag" datum))
+  (setq datum (replace-regexp-in-string "Wednesday" "Onsdag" datum))
+  (setq datum (replace-regexp-in-string "Thursday" "Torsdag" datum))
+  (setq datum (replace-regexp-in-string "Friday" "Fredag" datum))
+  (setq datum (replace-regexp-in-string "Saturday" "Lördag" datum))
+  (setq datum (replace-regexp-in-string "Sunday" "Söndag" datum))
+  (insert datum))
+
+;;; Commentary:
+;; Insert timestamp formatted to fit an org list.
 
 ;;; datumdag.el ends here
